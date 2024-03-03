2ccf3e1f0522a5147b77b99a34ab62fbe6040214
nwdaudt <nicholaswdaudt@gmail.com>
2024-03-03 19:29:16 +1300

    update code for GLMs including season and year as predictors, and then updated plots

--- scripts/05_data-analysis_univariate-Bernoulli-models.R
+++ scripts/05_data-analysis_univariate-Bernoulli-models.R
@@ -13,11 +13,6 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-# library(ggExtra)
-# library(patchwork)
-# library(RColorBrewer)
-# library(colorspace)
-# library(cowplot)
 
 ## Read data ####
 data <- read.csv("./data-processed/all_data_long.csv")[, -1]
@@ -31,10 +26,6 @@ data$taiaroa_east <-
                     "TaiaroaEast30.35km", "TaiaroaEast35.40km",
                     "TaiaroaEast40.45km", "TaiaroaEast45.50km",
                     "TaiaroaEast50.55km", "TaiaroaEast55.60km"))
-
-data$direction <- 
-  factor(data$direction,
-         levels = c("eastward", "westward"))
 
 data$season <- 
   factor(data$season,
@@ -77,85 +68,75 @@ long_data_pa <-
   tidyr::pivot_longer(cols = c(black_backed_gull:yellow_eye_penguin),
                       names_to = "species",
                       values_to = "p_a") %>% 
-  # and transform counts in presence-absence (p_a) [0,1]
+  # and transform counts to presence-absence ("p_a") [0,1]
   dplyr::mutate(p_a = replace(p_a, p_a > 0, 1)) %>% 
-  # filter away year 2012
-  dplyr::filter(year > 2012)
+  # Remove year 2012
+  dplyr::filter(year > 2012)%>% 
+  # Create 'dist_coast', with 'centroid' distance from Taiaroa Head
+  dplyr::mutate(dist_coast = case_when(
+    taiaroa_east == "TaiaroaEast0.5km" ~ 2.5,
+    taiaroa_east == "TaiaroaEast5.10km" ~ 7.5,
+    taiaroa_east == "TaiaroaEast10.15km" ~ 12.5, 
+    taiaroa_east == "TaiaroaEast15.20km" ~ 17.5,
+    taiaroa_east == "TaiaroaEast20.25km" ~ 22.5, 
+    taiaroa_east == "TaiaroaEast25.30km" ~ 27.5,
+    taiaroa_east == "TaiaroaEast30.35km" ~ 32.5, 
+    taiaroa_east == "TaiaroaEast35.40km" ~ 37.5,
+    taiaroa_east == "TaiaroaEast40.45km" ~ 42.5, 
+    taiaroa_east == "TaiaroaEast45.50km" ~ 47.5,
+    taiaroa_east == "TaiaroaEast50.55km" ~ 52.5, 
+    taiaroa_east == "TaiaroaEast55.60km" ~ 57.5,
+    .default = TRUE
+  ), .after = taiaroa_east)
+
+spp <- unique(long_data_pa$species)
 
 ## Bernoulli GLM (y ~ dist_coast, per year) ####
 
-spp <- unique(long_data_pa$species)
-years <- unique(long_data_pa$year)
+# List to save models
+sp_Bernoulli_year_models <- list()
 
+# Dataframe to save fitted values and plot
 dfs_yfit_year_sp <- data.frame()
 
 for (sp in spp) {
   
-  list_yfit_year <- list()
-  
-  # Get 'sp' vector
+  # Get 'sp' as a character vector
   sp_i <- as.character(sp)
   
-  # Filter
+  # Filter 'sp_i'
   tmp <- 
     long_data_pa %>% 
-    dplyr::filter(species == sp_i) %>% 
-    dplyr::mutate(dist_coast = case_when(
-      taiaroa_east == "TaiaroaEast0.5km" ~ 2.5,
-      taiaroa_east == "TaiaroaEast5.10km" ~ 7.5,
-      taiaroa_east == "TaiaroaEast10.15km" ~ 12.5, 
-      taiaroa_east == "TaiaroaEast15.20km" ~ 17.5,
-      taiaroa_east == "TaiaroaEast20.25km" ~ 22.5, 
-      taiaroa_east == "TaiaroaEast25.30km" ~ 27.5,
-      taiaroa_east == "TaiaroaEast30.35km" ~ 32.5, 
-      taiaroa_east == "TaiaroaEast35.40km" ~ 37.5,
-      taiaroa_east == "TaiaroaEast40.45km" ~ 42.5, 
-      taiaroa_east == "TaiaroaEast45.50km" ~ 47.5,
-      taiaroa_east == "TaiaroaEast50.55km" ~ 52.5, 
-      taiaroa_east == "TaiaroaEast55.60km" ~ 57.5,
-      .default = TRUE
-    ), .after = taiaroa_east)
+    dplyr::filter(species == sp_i)
   
-  for(year in years){
-    
-    year_i <- year
-    
-    tmp_yr <- tmp %>% filter(year == year_i)
-    
-    glm_tmp <- glm(p_a ~ dist_coast, data = tmp_yr, family = binomial(link = "logit"))
-    
-    list_yfit_year[[as.character(year_i)]] <- 
-      data.frame(taiaroa_east = tmp_yr$taiaroa_east,
-                 yfit = predict(glm_tmp,type = "response")) %>% 
-      dplyr::group_by(taiaroa_east) %>% 
-      dplyr::summarise(yfit_avg = mean(yfit))
-  }
+  # Fit the model
+  glm_tmp <- glm(p_a ~ dist_coast + year, data = tmp, family = binomial(link = "logit"))
+  
+  sp_Bernoulli_year_models[[sp_i]] <- glm_tmp
   
   df_yfit_year_sp <- 
-    dplyr::bind_rows(list_yfit_year, .id = "id") %>% 
-    dplyr::mutate(species = sp_i)
+    cbind(tmp, 
+          data.frame(yfit = predict(glm_tmp,type = "response")))
   
-  dfs_yfit_year_sp <- rbind(dfs_yfit_year_sp, df_yfit_year_sp)
+  dfs_yfit_year_sp <- rbind(dfs_yfit_year_sp, 
+                            df_yfit_year_sp)
   
   # Clean environment
-  rm("list_yfit_year", 
-     "sp", "sp_i", "tmp", 
-     "year", "year_i", "tmp_yr", "glm_tmp",
-     "df_yfit_year_sp")
+  rm("sp", "sp_i", "tmp", "glm_tmp", "df_yfit_year_sp")
 }
 
-rm("spp", "years")
+## Save model results
+saveRDS(sp_Bernoulli_year_models,
+        "./results/glm-Bernoulli_spp-dist-coast-years.rds")
 
-# unique(dfs_yfit_year_sp$id)
-# unique(dfs_yfit_year_sp$species)
-
+## Plot results
 plot_prob_occ_species_year <- 
   ggplot(data = dfs_yfit_year_sp,
-       aes(x = taiaroa_east, y = yfit_avg, group = id, colour = id)) +
+       aes(x = taiaroa_east, y = yfit, group = as.factor(year), colour = as.factor(year))) +
   geom_line() + 
   scale_color_brewer(palette = "Reds") +
   scale_x_discrete(labels = c("0-5 km", "", "", "", "", "25-30 km", "", "", "", "", "", "55-60 km")) +
-  ylab("Yearly average predicted probability of occurrence") + xlab("") +
+  ylab("Predicted probability of occurrence") + xlab("") +
   facet_wrap(~ species, scales = "fixed") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
@@ -171,87 +152,62 @@ ggsave(plot_prob_occ_species_year,
        filename = "./results/glm-Bernoulli_spp-years.pdf",
        height = 25, width = 40, units = "cm", dpi = 300)
 
+rm("sp_Bernoulli_year_models", "dfs_yfit_year_sp",
+   "plot_prob_occ_species_year")
+
 ## Bernoulli GLM (y ~ dist_coast, per season) ####
 
-spp <- unique(long_data_pa$species)
-seasons <- unique(long_data_pa$season)
+# List to save models
+sp_Bernoulli_seasons_models <- list()
 
-dfs_yfit_season_sp <- data.frame()
+# Dataframe to save fitted values and plot
+dfs_yfit_seasons_sp <- data.frame()
 
 for (sp in spp) {
   
-  list_yfit_season <- list()
-  
-  # Get 'sp' vector
+  # Get 'sp' as a character vector
   sp_i <- as.character(sp)
   
-  # Filter
+  # Filter 'sp_i'
   tmp <- 
     long_data_pa %>% 
-    dplyr::filter(species == sp_i) %>% 
-    dplyr::mutate(dist_coast = case_when(
-      taiaroa_east == "TaiaroaEast0.5km" ~ 2.5,
-      taiaroa_east == "TaiaroaEast5.10km" ~ 7.5,
-      taiaroa_east == "TaiaroaEast10.15km" ~ 12.5, 
-      taiaroa_east == "TaiaroaEast15.20km" ~ 17.5,
-      taiaroa_east == "TaiaroaEast20.25km" ~ 22.5, 
-      taiaroa_east == "TaiaroaEast25.30km" ~ 27.5,
-      taiaroa_east == "TaiaroaEast30.35km" ~ 32.5, 
-      taiaroa_east == "TaiaroaEast35.40km" ~ 37.5,
-      taiaroa_east == "TaiaroaEast40.45km" ~ 42.5, 
-      taiaroa_east == "TaiaroaEast45.50km" ~ 47.5,
-      taiaroa_east == "TaiaroaEast50.55km" ~ 52.5, 
-      taiaroa_east == "TaiaroaEast55.60km" ~ 57.5,
-      .default = TRUE
-    ), .after = taiaroa_east)
+    dplyr::filter(species == sp_i)
   
-  for(season in seasons){
-    
-    season_i <- season
-    
-    tmp_season <- tmp %>% filter(season == season_i)
-    
-    glm_tmp <- glm(p_a ~ dist_coast, data = tmp_season, family = binomial(link = "logit"))
-    
-    list_yfit_season[[as.character(season_i)]] <- 
-      data.frame(taiaroa_east = tmp_season$taiaroa_east,
-                 yfit = predict(glm_tmp,type = "response")) %>% 
-      dplyr::group_by(taiaroa_east) %>% 
-      dplyr::summarise(yfit_avg = mean(yfit))
-  }
+  # Fit the model
+  glm_tmp <- glm(p_a ~ dist_coast + season, data = tmp, family = binomial(link = "logit"))
   
-  df_yfit_season_sp <- 
-    dplyr::bind_rows(list_yfit_season, .id = "id") %>% 
-    dplyr::mutate(species = sp_i)
+  sp_Bernoulli_seasons_models[[sp_i]] <- glm_tmp
   
-  dfs_yfit_season_sp <- rbind(dfs_yfit_season_sp, df_yfit_season_sp)
+  df_yfit_seasons_sp <- 
+    cbind(tmp, 
+          data.frame(yfit = predict(glm_tmp,type = "response")))
+  
+  dfs_yfit_seasons_sp <- rbind(dfs_yfit_seasons_sp, 
+                               df_yfit_seasons_sp)
   
   # Clean environment
-  rm("list_yfit_season", 
-     "sp", "sp_i", "tmp", 
-     "season", "season_i", "tmp_season", "glm_tmp",
-     "df_yfit_season_sp")
+  rm("sp", "sp_i", "tmp", "glm_tmp", "df_yfit_seasons_sp")
 }
 
-rm("spp", "seasons")
+## Save model results
+saveRDS(sp_Bernoulli_seasons_models,
+        "./results/glm-Bernoulli_spp-dist-coast-seasons.rds")
 
-# unique(dfs_yfit_season_sp$id)
-# unique(dfs_yfit_season_sp$species)
-
+## Plot results
 plot_prob_occ_species_seasons <- 
-  ggplot(data = dfs_yfit_season_sp,
-         aes(x = taiaroa_east, y = yfit_avg, group = id, colour = id)) +
+  ggplot(data = dfs_yfit_seasons_sp,
+         aes(x = taiaroa_east, y = yfit, group = season, colour = season)) +
   geom_line() + 
-  scale_color_manual(values = c("#4E79A7", "#F28E2B", "#E15759", "#76B7B2"),
-                    name = "Season") +
+  scale_color_manual(values = c("#4E79A7", "#F28E2B", "#E15759", "#76B7B2")) +
   scale_x_discrete(labels = c("0-5 km", "", "", "", "", "25-30 km", "", "", "", "", "", "55-60 km")) +
-  ylab("Seasonal average predicted probability of occurrence") + xlab("") +
+  ylab("Predicted probability of occurrence") + xlab("") +
   facet_wrap(~ species, scales = "fixed") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         axis.text = element_text(size = 10, colour = "black"),
         axis.title.y = element_text(size = 12, colour = "black"),
         axis.line = element_line(colour = "black"),
+        legend.title = element_blank(),
         legend.text = element_text(size = 13),
         legend.position = c(0.8, 0.05)) +
   guides(colour = guide_legend(nrow = 1))
@@ -259,3 +215,4 @@ plot_prob_occ_species_seasons <-
 ggsave(plot_prob_occ_species_seasons,
        filename = "./results/glm-Bernoulli_spp-seasons.pdf",
        height = 25, width = 40, units = "cm", dpi = 300)
+
