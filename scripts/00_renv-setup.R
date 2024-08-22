454e21fefeded41e1adbce44b1bd259c6dcce609
nwdaudt <nicholaswdaudt@gmail.com>
2024-08-22 17:41:03 +1200

    initialised {renv} and updated script '00'

--- scripts/00_renv-setup.R
+++ scripts/00_renv-setup.R
@@ -4,13 +4,11 @@
 ## -------------------------------------------------------------------------- ##
 ## -------------------------------------------------------------------------- ##
 
-# renv::init()
-# renv::snapshot()
 
 ## If you don't have {renv} installed, please uncomment 
 ## the line below and install the package
 
-# install.packages("renv")
+# install.packages("renv", "1.0.3")
 
 ## Then, just run the following command, which will restore the R environment 
 ## using the same package versions as we used
@@ -19,3 +17,17 @@ renv::restore()
 
 ## If you're not familiarised with {renv}, please have a look at:
 ## https://rstudio.github.io/renv/articles/renv.html
+
+## -------------------------------------------------------------------------- ##
+## -------------------------------------------------------------------------- ##
+##
+## NOTE: 
+##
+## Package versions and their dependencies were captured using {renv} and 
+## locked in the `lock.file` file.
+##
+## If you install the packages by yourself, we cannot guarantee their versions
+## will be same as we used in this Rproject.
+##
+## -------------------------------------------------------------------------- ##
+## -------------------------------------------------------------------------- ##
